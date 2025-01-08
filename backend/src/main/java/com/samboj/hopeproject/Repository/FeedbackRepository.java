0af9145e518dcd52b8b20b79d0f1a99826b1959d
Abdou KANDJI <abdou.kandji@efrei.net>
2025-01-08 20:06:56 +0100

    ressource & feedback

--- backend/src/main/java/com/samboj/hopeproject/Repository/FeedbackRepository.java
+++ backend/src/main/java/com/samboj/hopeproject/Repository/FeedbackRepository.java
@@ -1,11 +1,13 @@
 package com.samboj.hopeproject.Repository;
 
 import com.samboj.hopeproject.Modele.Feedback;
+import com.samboj.hopeproject.Modele.Ressource;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
 
 public interface FeedbackRepository extends JpaRepository<Feedback, Long> {
-    List<Feedback> findByRessource_Id(Long RessourceId);
+    List<Feedback> findByRessource(Ressource ressource);
+    long countByRessource(Ressource ressource);
 }
 
